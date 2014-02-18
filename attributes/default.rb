5f12c41280d24f119c896dc528237ba392793291
Ben Langfeld <ben@langfeld.me>
2014-02-18 14:54:34 -0300

    Remove unused attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,4 @@
 default['unimrcp']['version'] = '1.1.0'
-default['unimrcp']['packages'] = %w{pkg-config build-essential}
 default['unimrcp']['install_dir'] = '/usr/local/unimrcp'
 default['unimrcp']['install_flite'] = false
 default['unimrcp']['install_pocketsphinx'] = false
